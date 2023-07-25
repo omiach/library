c3dcd8b216ce77d4cb5b51e22bbb9de571b2eac6
Alex Reznichenko <alexey@cloud9hr.com>
2023-07-25 17:39:59 +0300

    save

---
+++ Db/Functions/CountOfBooksOfAuthor.sql
@@ -0,0 +1,27 @@
+﻿CREATE FUNCTION [dbo].[CountOfBooksOfAuthor]
+(
+	@AuthorId int = 0
+)
+RETURNS INT
+AS
+BEGIN
+	RETURN
+	(
+		SELECT TOP (1)
+			[AuthorBooksCount].[Count]
+		FROM
+			(SELECT
+				1 as [Order]
+				,COUNT([BookId]) as [Count]
+				FROM [AuthorsBooks] 
+				WHERE [AuthorId] = @AuthorId
+				GROUP BY AuthorId
+			UNION ALL
+				SELECT 
+				2
+				,0
+			) as AuthorBooksCount
+		WHERE [AuthorBooksCount].[Count] IS NOT NULL
+		ORDER BY [AuthorBooksCount].[Order]
+	)
+END
